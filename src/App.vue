38aa43a429ef7412e4ab4eb43cb05e2c0bd20a9b
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-11-17 15:58:29 +0100

    milestone 4
    api response

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,32 @@
 <script>
+import axios from 'axios';
 
+export default {
+    name: 'App',
+    data() {
+        return {
+            base_link: 'http://127.0.0.1:8000/',
+            projects: []
+        }
+    },
+    mounted() {
+        axios
+            .get(this.base_link + 'api/projects')
+            .then(response => {
+                this.projects = response.data.result.data;
+                console.log(this.projects);
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+}
 </script>
 
 <template>
-    <h1 class="text-danger">Test</h1>
+    <h1>All projects</h1>
+    <ul>
+        <li v-for="project in projects">
+            {{ project.title }}</li>
+    </ul>
 </template>
