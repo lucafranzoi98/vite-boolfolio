877cb5f54b0b9fe4bf00459497bbf4d02c47c127
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-11-17 17:36:16 +0100

    milestone 5
    add ProjectCard component

--- src/App.vue
+++ src/App.vue
@@ -30,35 +30,20 @@ export default {
 <template>
     <h1 class="text-center my-3">All projects</h1>
 
-    <div class="container">
+    <div class="container mb-3">
         <div class="row row-cols-3 g-3">
             <div class="col" v-for="project in projects">
-                <div class="card h-100">
-                    <img v-if="project.image.includes('http')" class="card-img-top" :src="project.image">
-                    <img v-else-if="!project.image.includes('http')" class="card-img-top" :src="base_link + 'storage/' + project.image">
-                    <div v-else class="m-3">N/A</div>
-                    <div class="card-body">
-                        <h5 class="card-title">{{ project.title }}</h5>
-                        <h6 class="card-subtitle text-muted mb-2" v-if="project.type">
-                            {{ project.type.name }}
-                        </h6>
-                        <p class="card-text">{{ project.description }}</p>
-                        <div>
-                            <span>Code link: </span>
-                            <a :href="project.code_link" target="_blank" class="card-link">{{ project.code_link }}</a>
-                        </div>
-                        <div>
-                            <span>Preview link: </span>
-                            <a :href="project.preview_link" target="_blank" class="card-link">{{ project.preview_link }}</a>
-                        </div>
-                        <ul class="list-unstyled d-flex gap-1 mt-3" v-if="project.technologies">
-                            <li class="badge bg-dark" v-for="technology in project.technologies">{{ technology.name }}</li>
-                        </ul>
+                
 
-                    </div>
-                </div>
-
-                <!-- <ProjectCard  :title="project.title" :type="project.type ? project.type.name : ''" :description="project.description" :code_link="project.code_link" :preview_link="project.preview_link" :technologies="project.technologies" :technology_name="technology.name"></ProjectCard> -->
+                <ProjectCard :image="project.image"
+                :base_link="base_link"
+                :title="project.title"
+                :type="project.type"
+                :description="project.description"
+                :code_link="project.code_link"
+                :preview_link="project.preview_link"
+                :technologies="project.technologies"
+                ></ProjectCard>
             </div>
         </div>
     </div>
--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -2,17 +2,41 @@
 export default {
     name: 'ProjectCard',
     props: {
+        image: String,
+        base_link: String,
         title: String,
-        type: String,
+        type: Object,
         description: String,
         code_link: String,
         preview_link: String,
-        technologies: Array,
-        technology_name: String
+        technologies: Array
     }
 }
 </script>
 
 <template>
+    <div class="card h-100">
+        <img v-if="image.includes('http')" class="card-img-top" :src="image">
+        <img v-else-if="!image.includes('http')" class="card-img-top" :src="base_link + 'storage/' + image">
+        <div v-else class="m-3">N/A</div>
+        <div class="card-body">
+            <h5 class="card-title">{{ title }}</h5>
+            <h6 class="card-subtitle text-muted mb-2" v-if="type">
+                {{ type.name }}
+            </h6>
+            <p class="card-text">{{ description }}</p>
+            <div>
+                <span>Code link: </span>
+                <a :href="code_link" target="_blank" class="card-link">{{ code_link }}</a>
+            </div>
+            <div>
+                <span>Preview link: </span>
+                <a :href="preview_link" target="_blank" class="card-link">{{ preview_link }}</a>
+            </div>
+            <ul class="list-unstyled d-flex gap-1 mt-3" v-if="technologies">
+                <li class="badge bg-dark" v-for="technology in technologies">{{ technology.name }}</li>
+            </ul>
 
+        </div>
+    </div>
 </template>
